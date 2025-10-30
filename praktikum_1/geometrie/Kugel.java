43c8c1ed11b30c0d44378d631d645441edebffda
toxypiks <HannaHinkelbein89@gmx.de>
2025-10-30 10:14:27 +0100

    Start exercise one

---
+++ praktikum_1/geometrie/Kugel.java
@@ -0,0 +1,31 @@
+package geometrie;
+import java.lang.Math;
+
+public class Kugel {
+
+    private double radius;
+
+    public Kugel(double radius) {
+        this.radius = radius;
+    };
+
+    public double getRadius()
+    {
+        return radius;
+    }
+
+    public double getDurchmesser()
+    {
+        return radius*2;
+    }
+
+    public double getVolumen()
+    {
+        return 4.0/3.0*Math.PI*Math.pow(radius,3);
+    }
+
+    public double getOberflaeche()
+    {
+        return 4*Math.PI*Math.pow(radius,2);
+    }
+}
---
+++ praktikum_1/geometrie/Kugeln.java
@@ -0,0 +1,10 @@
+package geometrie;
+
+public class Kugeln {
+
+    public static void main (String[] args) {
+
+        Kugel meine_kugel = new Kugel(2.0);
+        System.out.println("test");
+    }
+}
