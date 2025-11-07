3c4a598594f58ed7b7234ac8865625c4d8bfb2b3
toxypiks <HannaHinkelbein89@gmx.de>
2025-11-07 17:33:43 +0100

    Add all requested functionalities to UtilsForArrays class

--- praktikum_2/project/UtilsForArrays.java
+++ praktikum_2/project/UtilsForArrays.java
@@ -1,21 +1,37 @@
 package project;
 import java.util.Arrays;
+import java.lang.Math;
 
 public class UtilsForArrays {
 
     public static int[] createIntArray(int minimum, int maximum) {
         int[] new_array = new int[maximum - minimum + 1];
-        int start = minimum;
         for(int i = 0; i < new_array.length; i++)
             {
-                new_array[i] = start;
-                start++;
+                new_array[i] = minimum;
+                minimum++;
             }
         return new_array;
     }
 
+    public static int[] quadriere(int[] array_to_square) {
+        int[] squared_array = new int[array_to_square.length];
+        for(int i = 0; i < squared_array.length; ++i)
+            {
+                squared_array[i] = (int) Math.pow(array_to_square[i],2);
+            }
+        return squared_array;
+    }
+
+    public static void arrayInhaltAusgeben(int[] array_to_print) {
+        System.out.println(Arrays.toString(array_to_print));
+    }
+
     public static void main (String[] args) {
         int[] my_array = UtilsForArrays.createIntArray(2, 7);
-        System.out.println(Arrays.toString(my_array));
+        UtilsForArrays.arrayInhaltAusgeben(my_array);
+
+        int[] squared_array = UtilsForArrays.quadriere(my_array);
+        UtilsForArrays.arrayInhaltAusgeben(squared_array);
     }
 }
