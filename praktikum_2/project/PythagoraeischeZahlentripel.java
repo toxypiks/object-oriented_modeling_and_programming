f4a281b0a1708d8237f489bd1f2810b43da1cb8a
toxypiks <HannaHinkelbein89@gmx.de>
2025-11-18 15:52:11 +0100

    Refactor code

--- praktikum_2/project/PythagoraeischeZahlentripel.java
+++ praktikum_2/project/PythagoraeischeZahlentripel.java
@@ -7,23 +7,24 @@ public class PythagoraeischeZahlentripel
     private int[] squared_numbers_to_check;
     private int found_zahlentripel;
 
-    public PythagoraeischeZahlentripel(final int minimum, final int maximum)
+    public PythagoraeischeZahlentripel(int minimum, int maximum)
     {
         this.numbers_to_check = UtilsForArrays.createIntArray(minimum, maximum);
         this.squared_numbers_to_check = UtilsForArrays.quadriere(this.numbers_to_check);
-        this.found_zahlentripel = 0;
+        UtilsForArrays.arrayInhaltAusgeben(this.squared_numbers_to_check);
+        berechne();
     }
 
-    public boolean istQuadratzahl(int number)
+    private boolean istQuadratzahl(int number)
     {
         if (number < 0) {
             return false; //a squared number can not be negative
         }
-        double sqrt_of_number = Math.floor(Math.sqrt(number));
+        int sqrt_of_number = (int) Math.floor(Math.sqrt(number));
         return sqrt_of_number*sqrt_of_number == number;
     }
 
-    public void berechne()
+    private void berechne()
     {
         for(int i = 0; i < this.squared_numbers_to_check.length - 1; i++) {
             for(int j = 0; j < this.squared_numbers_to_check.length; j++) {
--- praktikum_2/project/PythagoraeischeZahltripelMain.java
+++ praktikum_2/project/PythagoraeischeZahltripelMain.java
@@ -6,11 +6,9 @@ public class PythagoraeischeZahltripelMain
     public static void main (String[] args)
     {
         PythagoraeischeZahlentripel triple_eins = new PythagoraeischeZahlentripel(1, 1000);
-        triple_eins.berechne();
         triple_eins.gibAnzahlAus();
 
         PythagoraeischeZahlentripel triple_zwei = new PythagoraeischeZahlentripel(1001, 2000);
-        triple_zwei.berechne();
         triple_zwei.gibAnzahlAus();
     }
 }
