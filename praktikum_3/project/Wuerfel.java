a7cebae9e72cd437e9d83b5fb2750cf979fdb19d
toxypiks <HannaHinkelbein89@gmx.de>
2025-11-21 18:41:38 +0100

    Implement wuerfel class

--- praktikum_3/project/Wuerfel.java
+++ praktikum_3/project/Wuerfel.java
@@ -1,6 +1,37 @@
 package project;
 
+import java.lang.Math;
+
 public class Wuerfel
 {
+    private double seitenlaenge;
 
+    public Wuerfel(double seitenlaenge) throws GeometrischesObjektException {
+        if(seitenlaenge < 0) {
+            throw new GeometrischesObjektException("Wuerfel: Parameter seitenlaenge < 0");
+            //TODO collection improve message
+        }
+        else {
+            this.seitenlaenge = seitenlaenge;
+        }
+    }
+    public double getSeitenlaenge()
+    {
+        return this.seitenlaenge;
+    }
+
+    public double getVolumen()
+    {
+        return Math.pow(this.seitenlaenge,3);
+    }
+
+    public double getOberflaeche()
+    {
+        return 6 * Math.pow(this.seitenlaenge, 2);
+    }
+
+    public String toString()
+    {
+        return ("Würfel mit " + "Seitenlänge = " + this.seitenlaenge + " Volumen = " + getVolumen() + " Oberfläche = " + getOberflaeche() + "\n");
+    }
 }
