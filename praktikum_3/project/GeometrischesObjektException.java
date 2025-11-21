b1cf22b0a5973a35d6c46da08d5033b58c6aed19
toxypiks <HannaHinkelbein89@gmx.de>
2025-11-21 18:32:13 +0100

    Implement exception class

--- praktikum_3/project/GeometrischesObjektException.java
+++ praktikum_3/project/GeometrischesObjektException.java
@@ -1,6 +1,12 @@
 package project;
 
-public class GeometrischesObjektException
-{
+public class GeometrischesObjektException extends Exception {
 
+    public GeometrischesObjektException(){
+        super();
+    }
+
+    public GeometrischesObjektException (String message) {
+        super("GeometrischesObjekt: " + message);
+    }
 }
