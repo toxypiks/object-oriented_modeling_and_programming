38f960f79976a9d94352131502785a214e745ddc
toxypiks <HannaHinkelbein89@gmx.de>
2025-11-21 19:14:51 +0100

    Implement quader class

--- praktikum_3/project/Quader.java
+++ praktikum_3/project/Quader.java
@@ -2,5 +2,57 @@ package project;
 
 public class Quader
 {
+    private double laenge;
+    private double breite;
+    private double hoehe;
 
+    public Quader(double laenge, double breite, double hoehe) throws GeometrischesObjektException {
+        if (laenge < 0) {
+            throw new GeometrischesObjektException("Quader: Parameter ");
+        } else {
+            this.laenge = laenge;
+        }
+
+        if (breite < 0) {
+            throw new GeometrischesObjektException("Quader: Parameter ");
+        } else {
+            this.breite = breite;
+        }
+
+        if (hoehe < 0) {
+            throw new GeometrischesObjektException("Quader: Parameter ");
+        } else {
+            this.hoehe = hoehe;
+        }
+    }
+
+    public double getLaenge()
+    {
+        return this.laenge;
+    }
+
+    public double getBreite()
+    {
+        return this.breite;
+    }
+
+    public double getHoehe()
+    {
+        return this.hoehe;
+    }
+
+    public double getVolumen()
+    {
+        return this.laenge*this.breite*this.hoehe;
+    }
+
+    public double getOberflaeche()
+    {
+        return (2*this.laenge*this.breite) + (2*this.laenge*this.hoehe) + (2*this.breite*this.hoehe);
+    }
+
+    public String toString()
+    {
+        return "Würfel mit" + " Länge = " + this.laenge + " Breite = " + this.breite + " Höhe = " + this.hoehe + " Volumen = " + getVolumen() + " Oberfläche = " + getOberflaeche();
+    }
 }
