c9797a3825074cded93241c95235639faa197741
toxypiks <HannaHinkelbein89@gmx.de>
2025-12-05 12:07:08 +0100

    Implement class composition structure

--- praktikum_4/project/BinarySearchTree.java
+++ praktikum_4/project/BinarySearchTree.java
@@ -1,3 +1,16 @@
 package project;
 
-public class BinarySearchTree{}
+public class BinarySearchTree {
+    private BinarySearchTreeNode root = null;
+
+    public BinarySearchTree() {}
+
+    public void add(double key)
+    {
+        root.add(key);
+    }
+
+    public int getHeight() {
+        return root.getHeight();
+    }
+}
--- praktikum_4/project/BinarySearchTreeNode.java
+++ praktikum_4/project/BinarySearchTreeNode.java
@@ -1,3 +1,21 @@
 package project;
 
-public class BinarySearchTreeNode{}
+public class BinarySearchTreeNode {
+
+    private BinarySearchTreeNode left = null;
+    private BinarySearchTreeNode right = null;
+    private double key;
+
+    public BinarySearchTreeNode() {}
+
+    public void add(double key)
+    {
+        //add key to tree
+    }
+
+    public int getHeight()
+    {
+        //return height of tree
+        return 0;
+    }
+}
