521b3d16b71a433443503d6ce3272e21539143f5
toxypiks <HannaHinkelbein89@gmx.de>
2025-12-05 18:28:01 +0100

    Create two binary search trees

--- praktikum_4/project/TestBinarySearchTree.java
+++ praktikum_4/project/TestBinarySearchTree.java
@@ -4,15 +4,26 @@ public class TestBinarySearchTree
 {
     public static void main (String[] args)
     {
-        System.out.println("Hello from new project");
+        BinarySearchTree tree_one = new BinarySearchTree();
+        tree_one.add(50.0);
+        tree_one.add(25.0);
+        tree_one.add(37.5);
+        tree_one.add(50.0);
+        tree_one.add(62.5);
+        tree_one.add(75.0);
+        tree_one.add(87.5);
 
-        BinarySearchTree my_tree = new BinarySearchTree();
-        my_tree.add(23);
-        my_tree.add(4);
-        my_tree.add(50);
-        my_tree.add(13);
-        my_tree.add(0);
-        my_tree.add(70);
-        my_tree.print();
+        System.out.printf("Height of tree one: %d\n", tree_one.getHeight());
+
+        BinarySearchTree tree_two = new BinarySearchTree();
+        tree_two.add(12.5);
+        tree_two.add(25.0);
+        tree_two.add(37.5);
+        tree_two.add(50.0);
+        tree_two.add(62.5);
+        tree_two.add(75.0);
+        tree_two.add(87.5);
+
+        System.out.printf("Height of tree two: %d\n", tree_two.getHeight());
     }
 }
