92562e7e4d62bc686eb9a45657ac63cb5dddfc1d
toxypiks <HannaHinkelbein89@gmx.de>
2025-12-20 11:32:53 +0100

    Call TreeMap comparator constructor

--- praktikum_5/project/WurfStatistikImplementation.java
+++ praktikum_5/project/WurfStatistikImplementation.java
@@ -1,8 +1,12 @@
 package project;
 import java.util.TreeMap;
+import java.lang.Math;
 
 public class WurfStatistikImplementation extends TreeMap<Integer, Integer> implements WurfStatistik
 {
+    public WurfStatistikImplementation() {
+        super((a, b) -> (int)Math.signum(b - a));
+    }
     public void wurf(Integer augenzahl)
     {
         put(augenzahl, getOrDefault(augenzahl, 0) +1);
