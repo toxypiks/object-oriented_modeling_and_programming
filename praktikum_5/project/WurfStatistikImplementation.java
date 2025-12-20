953b9feb74e15a0674d1bcdb73e7cfb59b150b45
toxypiks <HannaHinkelbein89@gmx.de>
2025-12-20 11:58:23 +0100

    Implement Treemap constructor with Comparator functional Interface to order keys in Treemap descending

--- praktikum_5/project/WurfStatistikImplementation.java
+++ praktikum_5/project/WurfStatistikImplementation.java
@@ -5,6 +5,9 @@ import java.lang.Math;
 public class WurfStatistikImplementation extends TreeMap<Integer, Integer> implements WurfStatistik
 {
     public WurfStatistikImplementation() {
+        // TreeMap constructor with Comparator functional interface
+        // Math.signum returns -1.0 if b - a < 0 and 1.0 if b - a > 0 and 0.0 if b = a
+        // results in descending order of keys in Treemap
         super((a, b) -> (int)Math.signum(b - a));
     }
     public void wurf(Integer augenzahl)
